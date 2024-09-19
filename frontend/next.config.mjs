1879aeffcf1ebbbca27cb631de3ec82d16198bd0
Adriana Rose <adriana.rose.dh@gmail.com>
2024-09-18 21:16:13 -0700

    image error fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['lucide-react'],
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  transpilePackages: ['lucide-react'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  images: {
+    domains: ['placeholder.com'],
+  },
+};
+
+export default nextConfig;
